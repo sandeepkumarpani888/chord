fdce03b786fef1e438f3a712126862a5c3c39d18
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-11-16 16:21:14 +0530

    chordRPC for simulating the whole chord protocol, this will help ease the communication and testing of the protocol.

---
+++ chordRPC/chordRPC.go
@@ -0,0 +1,9 @@
+package chordRPC
+
+// Channel based RPC to test and work with for chord.
+// Simulates a real network with drops and random errors as well.
+//
+//
+// Reference taken from: https://golang.org/src/net/rpc/server.go
+//
+// Needs to be tested as well.
